0b6850f8d2d764c2c3624289c32863e65b8733d1
Bechara-Alam <becharaalam65@gmail.com>
2026-04-23 19:15:27 +0300

    Add files via upload

---
+++ Api/Model/Appointment.cs
@@ -0,0 +1,21 @@
+﻿namespace HospitalAPI.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public int DoctorId { get; set; }
+
+        public int PatientId { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public string Status { get; set; }
+        public string? PaymentStatus { get; set; } = "pending";
+        public string? PaymentMethod { get; set; }
+        public string? ReferenceNumber { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ Api/Model/PatientUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿public class PatientUpdateDto
+{
+    public string? Phone { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? NationalId { get; set; }
+    public string? BloodType { get; set; }
+    public string? Allergies { get; set; }
+    public string? Diseases { get; set; }
+    public string? Medications { get; set; }
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+    public string? City { get; set; }
+}

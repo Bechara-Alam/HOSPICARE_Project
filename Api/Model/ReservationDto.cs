1236b53618ce53a8f3021b80805b4d2bb1e50452
maroun1287 <alammaroun60@gmail.com>
2026-04-24 21:13:56 +0300

    Add files via upload

---
+++ Api/Model/ReservationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalAPI.Models
+{
+    public class ReservationDto
+    {
+        public int RoomId { get; set; }
+
+        public string PatientName { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime EndDateTime { get; set; }
+    }
+}

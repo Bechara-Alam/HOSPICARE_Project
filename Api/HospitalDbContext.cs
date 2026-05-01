ef699fe7be21c3522c92b2b3bb496f1900470782
Bechara-Alam <becharaalam65@gmail.com>
2026-05-01 17:30:23 +0300

    Add files via upload

---
+++ Api/HospitalDbContext.cs
@@ -0,0 +1,27 @@
+﻿
+using HospitalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalAPI.Data
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<DoctorAvailability> DoctorAvailabilities { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<LabRequest> LabRequests { get; set; }
+        public DbSet<LabResult> LabResults { get; set; }
+        public DbSet<Notification> Notifications { get; set; }   // FIXED
+        public DbSet<StaffUser> StaffUsers { get; set; }
+    }
+}
